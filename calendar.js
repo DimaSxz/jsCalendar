c3ed4e2f25f4f608abd3208153a13a8b14d190bc
DimaSxz <DimaS300i@yandex.ru>
2019-07-03 14:09:00 +0300

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,4 +1,20 @@
-!(function(){
+!(() => {
+
+    const dayNames = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
+        monthNames = [ "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" ];
+
+    Date.prototype.getLocaleString = function() {
+        const currentMonth = this.getMonth();
+
+        let monthName = monthNames[currentMonth];
+
+        if(currentMonth == 2 || currentMonth == 7)
+            monthName += 'а';
+        else monthName = monthName.substring(0, monthName.length - 1) + 'я';
+
+        return this.getDate() + ' ' + monthName + ' ' + this.getFullYear();
+    };
+
     Date.prototype.getWeek = function() {
         const date = new Date(this.getTime());
         date.setHours(0, 0, 0, 0);
@@ -7,110 +23,147 @@
         return 1 + Math.round(((date.getTime() - week.getTime()) / 86400000 - 3 + (week.getDay() + 6) % 7) / 7);
     };
 
-    const Calendar = function(element, weekNums = false, time = Date.now(), neighbor = true) {
+    let actualDate = new Date();
+
+    const isActualDay = (year, month, day) =>
+        day === actualDate.getDate()
+        && month === actualDate.getMonth()
+        && year === actualDate.getFullYear()
+    ;
+
+
+    const isPast = (year, month, day) => new Date(year, month, day).getTime() < actualDate.getTime() && !isActualDay(year, month, day);
+
+    const renderCalendar = async (element, options) => {
+
+        const year = options.year,
+            month = options.month;
+
+        actualDate = new Date();
+
+        const date = new Date(year, month + 1, 0),
+            lastDate = date.getDate(),
+            lastDay = date.getDay(),
+            firstDay = new Date(year, month, 1).getDay()
+        ;
+
+        let unsafeTR = true;
+
+        let calendar = (options.wrapper ? '<div class="calendar__wrapper">' : '' ) + '<p class="calendar__month-title">'+ monthNames[date.getMonth()] + ' ' + date.getFullYear() + '</p><table class="calendar__table"><thead><tr>';
+
+        for(let i = 0; i < 7; ++i) {
+            calendar += '<th><span>' + dayNames[i] + '</span></th>';
+        }
+
+        calendar += '</tr></thead><tbody><tr>';
+
+        const range = firstDay == 0 ? 6 : firstDay;
+
+        for(let i = Number(firstDay != 0); i < range; ++i) {
+            calendar += '<td>&nbsp;</td>';
+        }
+
+        let currentYear = date.getFullYear(),
+            currentMonth = date.getMonth();
+
+        for(let i = 1; i <= lastDate; ++i) {
+            const dateValue = year + '-' + (month + 1) + '-' + i;
+            const isPastDate = isPast(currentYear, currentMonth, i);
+            calendar += '<td data-date="'
+                            + dateValue + '" class="'
+                        + (
+                            isActualDay(currentYear, currentMonth, i)
+                            ? 'calendar__day--current'
+                            : (isPastDate
+                                ? 'calendar__day--past'
+                                : 'calendar__day--future'
+                            )
+                        ) + '"><' + (options.selectebleDate && (!options.selectOnlyFutureDates || !isPastDate) ? 'button value="' + dateValue + '"' : 'span') + '>' + i + '</' + (options.selectebleDate ? 'button' : 'span' ) + '></td>';
+
+            if((i + firstDay - 1) % 7 == 0) {
+                calendar += '</tr>';
+                if(i != lastDate) {
+                    calendar += '<tr>';
+                } else unsafeTR = false;
+            }
+        }
+
+        if(unsafeTR) {
+            for(let i = lastDay; i < 7; ++i) {
+                calendar += '<td>&nbsp;</td>';
+            }
+            calendar += '</tr>';
+        }
+
+        calendar += '</tbody></table>' + (options.wrapper ? '</div>' : '');
+
+        element.innerHTML = calendar;
+
+        if(options.selectebleDate) {
+
+            const calendarCells = element.querySelectorAll('td[data-date]' + (options.selectOnlyFutureDates ? ':not(.calendar__day--past)' : '') + '>button');
+
+            for(const cell of calendarCells) {
+                cell.classList.add('calendar__day--btn');
+                cell.onfocus = options.selectFunction;
+            }
+        }
+    };
+
+    function Calendar (element, options = {
+        year: actualDate.getFullYear(),
+        month: actualDate.getMonth(),
+        // weekNumber: false,
+        wrapper: false,
+        selectebleDate: false,
+        selectOnlyFutureDates: true,
+        selectFunction: undefined,
+        controls: false,
+        // controlsElements
+    }) {
 
         if(!(element instanceof HTMLElement))
             throw Error('Calendar element is not specified');
 
-        const date = new Date(time);
-
-        const calendar = {
-            element,
-            weekNums,
-            date,
-            prevMonth: null,
-            nextMonth: null,
-            getFirstDay: function() {
-                return new Date(`${this.date.getFullYear()}-${this.date.getMonth() + 1}-1`).getDay();
-            },
-            getLastDay: function() {
-                const date = new Date(`${this.date.getFullYear()}-${this.date.getMonth() + 1}-1`);
-
-                date.setMonth(date.getMonth() + 1);
-                date.setDate(0);
-
-                return date.getDay();
-            },
-            getLastDate: function() {
-                const date = new Date(`${this.date.getFullYear()}-${this.date.getMonth() + 1}-1`);
-
-                date.setMonth(date.getMonth() + 1);
-                date.setDate(0);
-
-                return date.getDate();
-            },
-            renderCalendar: async function() {
-                const that = this;
-                const rows = Math.ceil(that.getLastDate() / 7) + ((that.getLastDay() - that.getFirstDay()) >= 0 ? 0 : 1),
-                    columns = that.weekNums ? 8 : 7;
-
-                let curDate = 1, allowData = false;
-
-                let table = '<div class=calendar__current-month><table><thead><tr>';
-
-                if(that.weekNums)
-                    table += '<th><span>№</span></th>';
-
-                table += '<th><span>Вс</span></th>'
-                        + '<th><span>Пн</span></th>'
-                        + '<th><span>Вт</span></th>'
-                        + '<th><span>Ср</span></th>'
-                        + '<th><span>Чт</span></th>'
-                        + '<th><span>Пт</span></th>'
-                        + '<th><span>Сб</span></th>'
-                        + '</tr></thead><tbody>';
-
-
-                for(let i = 0; i < rows; ++i) {
-                    table += '<tr>';
-                    for(let j = 0; j < columns; ++j) {
-                        table += '<td>';
-                            if(that.weekNums && j === 0) {
-                                table += '<span>';
-                                table += new Date(`${that.date.getFullYear()}-${that.date.getMonth() + 1}-${curDate + 1}`).getWeek();
-                                table += '</span>';
-                            } else {
-                                if(i === 0 && ((that.weekNums && j - 1 === that.getFirstDay()) || !that.weekNums && j === that.getFirstDay())) {
-                                    allowData = true;
-                                }
-                                else if(i === rows - 1 && curDate === that.getLastDate() + 1) {
-                                    break;
-                                }
-
-                                if(allowData) {
-                                    table += curDate !== that.date.getDate()
-                                        ? `<span>${curDate}</span>`
-                                        : `<span class=calendar__current-day>${curDate}</span>`;
-                                    curDate++;
-                                }
-                            }
-                        table += '</td>';
-                    }
-                    table += '</tr>';
-                }
-
-                table += '</tbody>';
-
-                that.element.innerHTML = table + '</table></div>';
-
-                if(neighbor) {
-                    const curElem = that.element.getElementsByClassName('calendar__current-month')[0];
-                    const prevElem = document.createElement("div");
-                    prevElem.className = "calendar__prev-month";
-                    const nextElem = document.createElement("div");
-                    nextElem.className = "calendar__next-month";
-                    that.element.insertBefore(prevElem, curElem);
-                    that.element.appendChild(nextElem);
-
-                    that.prevMonth = new Calendar(prevElem, that.weekNums, new Date(`${that.date.getFullYear()}-${that.date.getMonth()}-1`), false);
-                    that.nextMonth = new Calendar(nextElem, that.weekNums, new Date(`${that.date.getFullYear()}-${that.date.getMonth() + 2}-1`), false);
-                }
-            }
+        options = {
+            year: options.year || actualDate.getFullYear(),
+            month: options.month || actualDate.getMonth(),
+            // weekNumber: options.weekNumber || false,
+            wrapper: options.wrapper || false,
+            selectebleDate: options.selectebleDate || false,
+            selectOnlyFutureDates: options.selectOnlyFutureDates || true,
+            selectFunction: typeof options.selectFunction === "function" ? options.selectFunction : undefined
         };
 
-        calendar.renderCalendar();
+        renderCalendar(element, options);
 
-        return calendar;
-    };
+        return {
+            element,
+            options,
+            next: function () {
+                if(this.options.month == 11) {
+                    this.options.month = 0;
+                    this.options.year++;
+                } else this.options.month++;
+                renderCalendar(this.element, this.options);
+
+            },
+            prev: function () {
+                if(this.options.month == 0) {
+                    this.options.month = 11;
+                    this.options.year--;
+                } else this.options.month--;
+                renderCalendar(this.element, this.options);
+            },
+            getMonths: function () {
+                return {
+                    prev: monthNames[!this.options.month ? 11 : this.options.month - 1],
+                    current: monthNames[this.options.month],
+                    next: monthNames[this.options.month == 11 ? 0 : this.options.month + 1],
+                };
+            }
+        };
+    }
+
     window.Calendar = Calendar;
 })();
